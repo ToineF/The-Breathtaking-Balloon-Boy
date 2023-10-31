453ca38a9d3763ed5d73c796ad78ba74f9ef3678
ToineF <113095213+ToineF@users.noreply.github.com>
2023-10-31 14:04:17 +0100

    Wind Zone Base Force 🎍

--- Assets/Scripts/Character/CharacterControllerTest.cs
+++ Assets/Scripts/Character/CharacterControllerTest.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 [RequireComponent(typeof(CharacterController))] [HelpURL("https://antoine-foucault.itch.io/")]
 public class CharacterControllerTest: MonoBehaviour
@@ -11,19 +12,39 @@ public class CharacterControllerTest: MonoBehaviour
     [SerializeField] private string _animatorWalkBool;
     private CharacterController _characterController;
     private Vector3 _lastDirection;
+    private Vector3 _moveVector;
+    [SerializeField] private float Gravity;
+    public float Force;
+    private float _currentForce;
+    [SerializeField] [Range(0,1)] private float _startLerpValue;
+    [SerializeField] [Range(0,1)] private float _stopLerpValue;
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _currentForce = Force;
     }
 
-    void Update()
+    private void Update()
     {
         float xPosition = (Input.GetKey(KeyCode.RightArrow) ? 1 : 0) - (Input.GetKey(KeyCode.LeftArrow) ? 1 : 0);
         float zPosition = (Input.GetKey(KeyCode.UpArrow) ? 1 : 0) - (Input.GetKey(KeyCode.DownArrow) ? 1 : 0);
-        Vector3 moveVector = new Vector3(xPosition, 0, zPosition) * Speed;
-        SetAnimation(moveVector);
-        _characterController.Move(moveVector);
+        _moveVector = new Vector3(xPosition * Speed, 0, zPosition * Speed) * Time.deltaTime;
+        SetAnimation(_moveVector);
+        _characterController.Move(_moveVector);
+
+    }
+
+    void FixedUpdate()
+    {
+        float gravity = Gravity - Force;
+
+        _characterController.Move(Vector3.up * gravity * Time.deltaTime);
+
+        var force = Force;
+        var lerp = _currentForce >= 0 ? _stopLerpValue : _startLerpValue;
+        Force = Mathf.Lerp(Force, _currentForce, lerp);
+        if (Mathf.Abs(force - Force) < 0.001f && _currentForce != 0) _currentForce = 0;
     }
 
     private void SetAnimation(Vector3 moveVector)
@@ -31,5 +52,10 @@ public class CharacterControllerTest: MonoBehaviour
         if (moveVector != Vector3.zero) _lastDirection = moveVector;
         _animator.SetBool(_animatorWalkBool, moveVector != Vector3.zero);
         transform.LookAt(transform.position + moveVector);
+    }
+
+    public void SetForce(float value)
+    {
+        _currentForce = value;
     }
 }
--- Assets/Scripts/GPE/Wind Zone/WindZone.cs
+++ Assets/Scripts/GPE/Wind Zone/WindZone.cs
@@ -6,6 +6,7 @@ using System;
 [RequireComponent(typeof(Collider))]
 public class WindZone : MonoBehaviour
 {
+    [SerializeField][Tooltip("The magnitude of the applied force, as a positive number")] private float _forceMagnitude;
     [SerializeField] private float _airPercentageAddedOnContact;
     [SerializeField] private float _timeBetweenAdditions;
     private float _timer;
@@ -21,8 +22,17 @@ public class WindZone : MonoBehaviour
 
         if (other.GetComponent<CharacterControllerTest>() == null) return;
 
+        other.GetComponent<CharacterControllerTest>().SetForce(_forceMagnitude * Time.deltaTime);
         AirManager.Instance.AddAir(_airPercentageAddedOnContact);
         _timer = _timeBetweenAdditions;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<CharacterControllerTest>() == null) return;
+
+        if (Mathf.Abs(other.GetComponent<CharacterControllerTest>().Force) > 0.1f) return;
+        other.GetComponent<CharacterControllerTest>().SetForce(0);
     }
 
     private void Update()
