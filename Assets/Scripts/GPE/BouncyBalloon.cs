a23165a6ff94e900a7fb2f0af5864744e35c745a
AntoineF00 <113095213+ToineF@users.noreply.github.com>
2023-11-13 01:07:03 +0100

    Bouncy Balloon 🍴

---
+++ Assets/Scripts/GPE/BouncyBalloon.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class BouncyBalloon : MonoBehaviour
+{
+    [Header("Force")]
+    [SerializeField] private Vector3 _force;
+    [SerializeField] [Range(0, 1)] private float _forceAccel;
+    [SerializeField] [Range(0, 1)] private float _forceDecel;
+    [SerializeField] private Vector3 _vector3Up;
+
+    [Header("Timer")]
+    private bool _isPlayerIn;
+
+    [Header("Visual")]
+    [SerializeField] private float _scaleMultiplier = 1;
+    [SerializeField] private float _scaleTime;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.TryGetComponent(out CharacterControllerTest characterController)) return;
+        if (_isPlayerIn) return;
+        _isPlayerIn = true;
+        characterController.SetForce(_force, _forceAccel);
+        transform.DOComplete();
+        transform.DOPunchScale(_vector3Up * _scaleMultiplier, _scaleTime, 0, 0);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.TryGetComponent(out CharacterControllerTest characterController)) return;
+        _isPlayerIn = false;
+        characterController.SetForce(Vector3.zero, _forceDecel);
+
+    }
+}
