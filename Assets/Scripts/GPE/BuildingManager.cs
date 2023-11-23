ebb4cf5a6de2e5071616927a6894d47c4c870536
ToineF <113095213+ToineF@users.noreply.github.com>
2023-11-23 13:11:44 +0100

    Easier Previsualisation Buildings ⛳

--- Assets/Scripts/GPE/BuildingManager.cs
+++ Assets/Scripts/GPE/BuildingManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 using DG.Tweening;
 using BlownAway.Player;
 using AntoineFoucault.Utilities;
+using UnityEngine.ProBuilder.Shapes;
 
 namespace BlownAway.GPE.Buildings {
 
@@ -20,9 +21,10 @@ namespace BlownAway.GPE.Buildings {
 
         [Header("References")]
         [SerializeField] private GameObject _building;
-        [SerializeField] private GameObject _ghostBuilding;
         [SerializeField] private GameObject _balloon;
         [SerializeField] private GameObject _UIIsActivatable;
+        [SerializeField] private GameObject _buildingCubesParent;
+        [SerializeField] private Material _ghostMaterial;
 
         [Header("Properties")]
         [SerializeField] private float _buildingMoveSpeed = 1;
@@ -41,7 +43,7 @@ namespace BlownAway.GPE.Buildings {
         private float _lowerPositionY;
         private float _startUIY;
         private Vector3 _ballonBaseScale;
-        private int _startInflationLevel;
+        private GameObject _ghostBuilding;
 
 
         private void Awake()
@@ -58,6 +60,8 @@ namespace BlownAway.GPE.Buildings {
             _UIAirLevel.SetActive(false);
             _UIAirLevelArrow.transform.DOMoveY(_startUIY + _inflationLevel * _UIArrowincreaseHeight * (Screen.height / _referenceScreenHeight), 1);
             _ballonBaseScale = _balloon.transform.localScale;
+
+            SetUpBuildingCubes();
         }
 
         private void OnEnable()
@@ -84,8 +88,7 @@ namespace BlownAway.GPE.Buildings {
 
             _UIAirLevel.SetActive(!canPlayerMove);
             _ghostBuilding.SetActive(!canPlayerMove);
-            _startInflationLevel = _inflationLevel;
-            ResetGhostBuildingPosition();
+            _ghostBuilding.transform.position = new Vector3(_ghostBuilding.transform.position.x, _lowerPositionY + _inflationLevel * _airFloorHeight, _ghostBuilding.transform.position.z);
             if (canPlayerMove) MoveBuilding();
         }
 
@@ -93,21 +96,32 @@ namespace BlownAway.GPE.Buildings {
         {
             _inflationLevel = Mathf.Clamp(_inflationLevel + value, _minLevel, _maxLevel);
             _balloon.transform.DOScale(_ballonBaseScale * _balloonScaleLevel[_inflationLevel], _buildingMoveSpeed);
-            ResetGhostBuildingPosition();
-            _ghostBuilding.transform.DOMoveY(_lowerPositionY + _inflationLevel * _airFloorHeight, _buildingMoveSpeed).SetLoops(-1, LoopType.Restart);
-
             _UIAirLevelArrow.transform.DOMoveY(_startUIY + _inflationLevel * _UIArrowincreaseHeight * (Screen.height / _referenceScreenHeight), _buildingMoveSpeed);
-        }
 
-        private void ResetGhostBuildingPosition()
-        {
             _ghostBuilding.transform.DOKill();
-            _ghostBuilding.transform.position = new Vector3(_ghostBuilding.transform.position.x, _lowerPositionY + _startInflationLevel * _airFloorHeight, _ghostBuilding.transform.position.z);
+            _ghostBuilding.transform.DOMoveY(_lowerPositionY + _inflationLevel * _airFloorHeight, _buildingMoveSpeed);
+
         }
 
         private void MoveBuilding()
         {
             _building.transform.DOMoveY(_lowerPositionY + _inflationLevel * _airFloorHeight, _buildingMoveSpeed);
+        }
+
+        private void SetUpBuildingCubes()
+        {
+            _ghostBuilding = Instantiate(_building, _building.transform.position, _building.transform.rotation, _building.transform.parent);
+            int childCount = _ghostBuilding.transform.childCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                GameObject cube = _ghostBuilding.transform.GetChild(i).gameObject;
+                if (!cube.TryGetComponent(out Collider collider)) return;
+                Destroy(collider);
+                if (!cube.TryGetComponent(out MeshRenderer meshRenderer)) return;
+                meshRenderer.material = _ghostMaterial;
+            }
+            _ghostBuilding.SetActive(false);
+            _ghostBuilding.transform.localScale = _ghostBuilding.transform.localScale * 0.9999f;
         }
     }
 }
