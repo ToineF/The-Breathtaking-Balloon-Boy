28d15b0d3af62e2894fa6b57199258b33e503d2a
AntoineF00 <113095213+ToineF@users.noreply.github.com>
2023-12-01 22:51:44 +0100

    Inflatable Head 👕

--- Assets/Scripts/Character/BalloonBoyController.cs
+++ Assets/Scripts/Character/BalloonBoyController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class BalloonBoyController : MonoBehaviour
 {
+    [Header("Glide")]
     [SerializeField] private int _maxJumps;
     [SerializeField] private float _jumpForce;
     [SerializeField] private float _jumpTime;
@@ -13,28 +15,52 @@ public class BalloonBoyController : MonoBehaviour
     [Range(0, 1)][SerializeField] private float _jumpDecel;
     [SerializeField] private float _glideForce;
     [Range(0, 1)] [SerializeField] private float _glideAccel;
+
+    [Header("Air")]
+    [SerializeField] private float _maxAir;
+    [SerializeField] private float _airReductionSpeed;
+
+    [Header("Visuals")]
     [SerializeField] private GameObject _jumpFXPrefab;
+    [SerializeField] private GameObject _balloonVisual;
+    [SerializeField] private float _balloonScaleValue = 1f;
+    [SerializeField] private float _balloonScaleTime = 1f;
 
     private int _jumps;
+    private float _currentAir;
+    private Vector3 _balloonOriginalScale;
 
     private PlayerInputs _inputs;
 
     private void Awake()
     {
         _inputs = new PlayerInputs();
+    }
+
+    private void Start()
+    {
         _jumps = _maxJumps;
+        _currentAir = _maxAir;
+        _balloonOriginalScale = _balloonVisual.transform.localScale;
     }
 
     private void OnEnable()
     {
         _inputs.Enable();
-        _inputs.Player.TakePicture.performed += BalloonBump;
+        _inputs.Player.Action.performed += BalloonBump;
     }
 
     private void OnDisable()
     {
         _inputs.Disable();
-        _inputs.Player.TakePicture.performed -= BalloonBump;
+        _inputs.Player.Action.performed -= BalloonBump;
+    }
+
+    private void Update()
+    {
+        if (_currentAir < 0) return;
+            _currentAir -= _airReductionSpeed*Time.deltaTime;
+        if (_currentAir < 0) ResetBalloonScale();
     }
 
     private void BalloonBump(InputAction.CallbackContext context)
@@ -42,8 +68,7 @@ public class BalloonBoyController : MonoBehaviour
         if (_jumps <= 0) return;
 
         CharacterControllerTest.Instance.OnGroundEnter += PlayerEnterGround;
-        if (!CharacterControllerTest.Instance.IsGrounded) // also is force of wind is null
-            _jumps--;
+        _jumps--;
 
 
         Vector3 hit = Vector3.up * _jumpForce;
@@ -54,17 +79,28 @@ public class BalloonBoyController : MonoBehaviour
 
         Vector3 glideForce = Vector3.up * _glideForce;
         float glideAccel = _glideAccel;
-        //CharacterControllerTest.Instance.AddForce(glideForce, glideAccel);
+        CharacterControllerTest.Instance.AddForce(glideForce, glideAccel);
 
         Collider collider = CharacterControllerTest.Instance.GetComponent<Collider>();
-        GameObject.Instantiate(_jumpFXPrefab, collider.bounds.center - collider.bounds.extents.y * Vector3.up, _jumpFXPrefab.transform.rotation);
+        Instantiate(_jumpFXPrefab, collider.bounds.center - collider.bounds.extents.y * Vector3.up, _jumpFXPrefab.transform.rotation);
+
+        _balloonVisual.transform.DOScale(Vector3.one * _balloonScaleValue, _balloonScaleTime);
+
+        // Air
+        _currentAir = _maxAir;
     }
 
     private void PlayerEnterGround()
     {
-        Debug.Log("h");
         _jumps = _maxJumps;
         CharacterControllerTest.Instance.OnGroundEnter -= PlayerEnterGround;
+        ResetBalloonScale();
+
+    }
+
+    private void ResetBalloonScale()
+    {
         CharacterControllerTest.Instance.SetForce(Vector3.zero, 1);
+        _balloonVisual.transform.DOScale(_balloonOriginalScale, _balloonScaleTime);
     }
 }
