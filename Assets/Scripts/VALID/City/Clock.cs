7104cfc2456440db01ae7fdd81379495acb2af67
AntoineF00 <113095213+ToineF@users.noreply.github.com>
2024-05-26 23:39:17 +0200

    [PROGRAMMING] Kiosque Colliders + Gare Moving Needles ⤵

--- Assets/Scripts/VALID/City/Clock.cs
+++ Assets/Scripts/VALID/City/Clock.cs
@@ -5,11 +5,17 @@ public class Clock : MonoBehaviour
 {
     [SerializeField] private GameObject _minutesNeedle;
     [SerializeField] private GameObject _hoursNeedle;
+    [SerializeField] private Vector3 _rotationAxis;
+
+    private Vector3 _minuteStartAngle;
+    private Vector3 _hoursStartAngle;
 
     private void Start()
     {
         Debug.Log("HOURS: " + DateTime.Now.Hour);
         Debug.Log("MINUTES: " + DateTime.Now.Minute);
+        _minuteStartAngle = _minutesNeedle.transform.localEulerAngles;
+        _hoursStartAngle = _hoursNeedle.transform.localEulerAngles;
         UpdateRotation();
     }
 
@@ -22,8 +28,8 @@ public class Clock : MonoBehaviour
     {
         float minuteAngle = DateTime.Now.Minute / 60f * 360;
         float hourAngle = (DateTime.Now.Hour % 12 / 12f + minuteAngle/360f/12f) * 360;
-        Quaternion minutesRotation = Quaternion.Euler(new Vector3(0, -minuteAngle, 0));
-        Quaternion hoursRotation = Quaternion.Euler(new Vector3(0, -hourAngle, 0));
+        Quaternion minutesRotation = Quaternion.Euler(_minuteStartAngle + _rotationAxis * -minuteAngle);
+        Quaternion hoursRotation = Quaternion.Euler(_hoursStartAngle + _rotationAxis * -hourAngle);
         _minutesNeedle.transform.localRotation = minutesRotation;
         _hoursNeedle.transform.localRotation = hoursRotation;
     }
