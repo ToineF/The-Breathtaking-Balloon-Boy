613971af2828f002104460d11eccb7f96e853149
ToineF <113095213+ToineF@users.noreply.github.com>
2024-06-05 19:30:23 +0200

    [PROGRAMMING] Shop Item Sold Out 🕐

--- Assets/Scripts/VALID/City/Shop/ShopItem.cs
+++ Assets/Scripts/VALID/City/Shop/ShopItem.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using TMPro;
 using BlownAway.Collectibles;
 using System;
+using UnityEngine.UI;
 
 namespace BlownAway.City
 {
@@ -9,34 +10,61 @@ namespace BlownAway.City
     {
         public Action<CharacterCollectiblesManager> OnBuy;
 
-        [SerializeField] private int _price;
+        [SerializeField] private int[] _prices;
 
         [Header("References")]
-        [SerializeField] private TMP_Text _priceUI;
+        [SerializeField] private GameObject _priceUI;
+        [SerializeField] private TMP_Text _priceText;
         [SerializeField] private Color _affordablePriceColor = Color.white;
         [SerializeField] private Color _overpricedColor = Color.red;
+        [SerializeField] private Color _soldOutColor = Color.red;
+        [SerializeField] private Image _soldOutImage;
 
         private bool _isAffordable;
         private CharacterCollectiblesManager _player;
+        private int _currentItem = 0;
+        private bool _soldOut = false;
 
 
         private void Awake()
         {
-            _priceUI.text = _price.ToString();
+            _priceText.text = _prices[0].ToString();
         }
 
         public void UpdateUI(CharacterCollectiblesManager player)
         {
             _player = player;
-            _isAffordable = player.CurrentCoins >= _price;
-            _priceUI.color = _isAffordable ? _affordablePriceColor : _overpricedColor;
+            if (!_soldOut)
+            {
+                _isAffordable = player.CurrentCoins >= _prices[_currentItem];
+                _priceText.color = _isAffordable ? _affordablePriceColor : _overpricedColor;
+                _priceText.text = _prices[_currentItem].ToString();
+            } 
+            else
+            {
+                _priceText.color = _soldOutColor;
+                _priceText.text = "SOLD OUT";
+                _priceUI.SetActive(false);
+                _soldOutImage.gameObject.SetActive(true);
+            }
         }
 
         public void TryBuy()
         {
             if (!_isAffordable) return;
+            if (_soldOut) return;
 
-            _player.RemoveCoin(_price);
+            _player.RemoveCoin(_prices[_currentItem]);
+
+            if (_currentItem >= _prices.Length - 1)
+            {
+                _soldOut = true;
+
+            } else
+            {
+                _currentItem++;
+            }
+
             OnBuy?.Invoke(_player);
         }
     }
