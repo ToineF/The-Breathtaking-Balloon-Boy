bb12cc610fca22512f5ad4d27ce738b8850d5804
ToineF <113095213+ToineF@users.noreply.github.com>
2024-06-18 15:42:35 +0200

    [PROGRAMMING] Cutscene UI Skip Fix + Telepherique Layer Fix 🌕

--- Assets/Scripts/VALID/Cutscenes/UI/CutsceneSkipUI.cs
+++ Assets/Scripts/VALID/Cutscenes/UI/CutsceneSkipUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace BlownAway.Cutscenes
     {
@@ -6,10 +7,12 @@ namespace BlownAway.Cutscenes
     {
         [Header("Reference")]
         [SerializeField] private CutsceneManager _cutsceneManager;
+        [SerializeField] private Image _background;
 
         private void Update()
         {
             FillAmount = _cutsceneManager.NormalizedSkipCutsceneTime;
+            _background.color = new Color(1,1,1, FillAmount > 0.01f ? 1 : 0);
         }
     }
 }
